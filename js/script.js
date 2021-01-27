4fa5f348811782edbf201242b3baefd252819ac5
syuhada-ramadhan <suhadahada18@gmail.com>
2021-01-27 10:25:35 +0700

    Latihan menghapus elemen array pada javascript selesai

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// array hewan
+const hewan = ["Kambing", "Kerbau", "Kucing", "Sapi"];
+
+// method splice()
+hewan.splice(1, 1);
+
+// method shift()
+hewan.shift();
+
+// method pop()
+hewan.pop();
+
+// cetak array hewan
+console.log(hewan);
